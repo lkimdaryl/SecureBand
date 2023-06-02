8b6141b84cd60d2467411b4b43fdea1e46ac9fa0
lkimdaryl <78583414+lkimdaryl@users.noreply.github.com>
2023-06-01 21:39:39 -0700

    added functionality to new kids profile

--- application/server/public/dashboard.js
+++ application/server/public/dashboard.js
@@ -6,47 +6,46 @@ document.addEventListener("DOMContentLoaded", function () {
         create_profile();
     });
 
-    var kidsProfiles = document.querySelectorAll(".kids_profile")
-    kidsProfiles.forEach(function (profile) {
-        profile.addEventListener("click", function () {
-            edit_profile();
-        });
-    });
-
+//    var kidsProfiles = document.querySelectorAll(".kids_profile");
+//    kidsProfiles.forEach(function (profile) {
+//        profile.addEventListener("click", function () {
+//            edit_profile(profile);
+//        });
+//    });
 
     function closeForm() {
-        var overlay = document.querySelector('#overlay');
+        var overlay = document.querySelector("#overlay");
         overlay.remove();
-        body.style.overflow = 'auto'; // Restore scrolling
+        body.style.overflow = "auto"; // Restore scrolling
     }
 
-    function create_profile(){
-        var overlay = document.createElement('div');
-        overlay.id = 'overlay';
+    function create_profile() {
+        var overlay = document.createElement("div");
+        overlay.id = "overlay";
 
-        var form = document.createElement('form');
-        form.id = 'member_form';
+        var form = document.createElement("form");
+        form.id = "member_form";
 
-        var firstNameInput = document.createElement('input');
-        firstNameInput.type = 'text';
-        firstNameInput.placeholder = 'First Name';
-        firstNameInput.name = 'first_name'
+        var firstNameInput = document.createElement("input");
+        firstNameInput.type = "text";
+        firstNameInput.placeholder = "First Name";
+        firstNameInput.name = "first_name";
 
-        var lastNameInput = document.createElement('input');
-        lastNameInput.type = 'text';
-        lastNameInput.placeholder = 'Last Name';
-        lastNameInput.name = 'last_name'
+        var lastNameInput = document.createElement("input");
+        lastNameInput.type = "text";
+        lastNameInput.placeholder = "Last Name";
+        lastNameInput.name = "last_name";
 
-        var closeButton = document.createElement('button');
-        closeButton.id = 'close_button'
-        closeButton.textContent = 'X';
-        closeButton.addEventListener('click', function () {
-          closeForm();
+        var closeButton = document.createElement("button");
+        closeButton.id = "close_button";
+        closeButton.textContent = "X";
+        closeButton.addEventListener("click", function () {
+            closeForm();
         });
 
-        var submitButton = document.createElement('input');
-        submitButton.type = 'submit';
-        submitButton.value = 'Submit';
+        var submitButton = document.createElement("input");
+        submitButton.type = "submit";
+        submitButton.value = "Submit";
 
         form.appendChild(closeButton);
         form.appendChild(firstNameInput);
@@ -57,10 +56,10 @@ document.addEventListener("DOMContentLoaded", function () {
         body.appendChild(overlay);
 
         // Prevent scrolling when the form is open
-        body.style.overflow = 'hidden';
+        body.style.overflow = "hidden";
 
         // Handle form submission
-        form.addEventListener('submit', function (event) {
+        form.addEventListener("submit", function (event) {
             event.preventDefault();
             // Add your code here to handle form submission
             var firstName = firstNameInput.value;
@@ -68,66 +67,71 @@ document.addEventListener("DOMContentLoaded", function () {
             var parentId = getCookie("user_id");
 
             // Create a new kids_profile div
-            var newKidDiv = document.createElement('div');
-            newKidDiv.classList.add('kids_profile');
+            var newKidDiv = document.createElement("div");
+            newKidDiv.classList.add("kids_profile");
 
             // Create an image element
-            var imgElement = document.createElement('img');
-            imgElement.classList.add('profile_pic');
+            var imgElement = document.createElement("img");
+            imgElement.classList.add("profile_pic");
             imgElement.src = "../public/images/blank_profile.png"; // Set the image source here
 
             // Create a paragraph element with the kid's name
-            var pElement = document.createElement('p');
-            pElement.textContent = firstName + ' ' + lastName;
+            var pElement = document.createElement("p");
+            pElement.textContent = firstName + " " + lastName;
 
             // Append the image and paragraph elements to the new kids_profile div
             newKidDiv.appendChild(imgElement);
             newKidDiv.appendChild(pElement);
 
             // Append the new kids_profile div to the kidsSection
-            kidsSection = document.querySelector('#kids')
+            kidsSection = document.querySelector("#kids");
             kidsSection.appendChild(newKidDiv);
 
-            child_data = {first_name: firstName, last_name: lastName, parent_id: parentId}
-            fetch('/create_child', {
-                credentials: 'same-origin',
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(child_data)
+            // Add event listener to the new kids_profile div for editing
+            newKidDiv.addEventListener("click", function () {
+                edit_profile(newKidDiv);
+            });
+
+            child_data = { first_name: firstName, last_name: lastName, parent_id: parentId };
+            fetch("/create_child", {
+            credentials: "same-origin",
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(child_data),
             })
-            .then(response => response.json())
-            .then(response => {
-                console.log(response)
+            .then((response) => response.json())
+            .then((response) => {
+                console.log(response);
             });
             closeForm();
         });
     }
 
-    function edit_profile(){
-        var overlay = document.createElement('div');
-        overlay.id = 'overlay';
+    function edit_profile(profile) {
+        var overlay = document.createElement("div");
+        overlay.id = "overlay";
 
-        var form = document.createElement('form');
-        form.id = 'member_form';
+        var form = document.createElement("form");
+        form.id = "member_form";
 
-        var firstNameInput = document.createElement('input');
-        firstNameInput.type = 'text';
-        firstNameInput.placeholder = 'First Name';
+        var firstNameInput = document.createElement("input");
+        firstNameInput.type = "text";
+        firstNameInput.placeholder = "First Name";
 
-        var lastNameInput = document.createElement('input');
-        lastNameInput.type = 'text';
-        lastNameInput.placeholder = 'Last Name';
+        var lastNameInput = document.createElement("input");
+        lastNameInput.type = "text";
+        lastNameInput.placeholder = "Last Name";
 
-        var closeButton = document.createElement('button');
-        closeButton.id = 'close_button'
-        closeButton.textContent = 'X';
-        closeButton.addEventListener('click', function () {
-          closeForm();
+        var closeButton = document.createElement("button");
+        closeButton.id = "close_button";
+        closeButton.textContent = "X";
+        closeButton.addEventListener("click", function () {
+            closeForm();
         });
 
-        var submitButton = document.createElement('input');
-        submitButton.type = 'submit';
-        submitButton.value = 'Submit';
+        var submitButton = document.createElement("input");
+        submitButton.type = "submit";
+        submitButton.value = "Submit";
 
         form.appendChild(closeButton);
         form.appendChild(firstNameInput);
@@ -138,17 +142,21 @@ document.addEventListener("DOMContentLoaded", function () {
         body.appendChild(overlay);
 
         // Prevent scrolling when the form is open
-        body.style.overflow = 'hidden';
+        body.style.overflow = "hidden";
+
+        // Prepopulate form fields with existing data
+        firstNameInput.value = profile.querySelector("p").textContent.split(" ")[0];
+        lastNameInput.value = profile.querySelector("p").textContent.split(" ")[1];
 
         // Handle form submission
-        form.addEventListener('submit', function (event) {
+        form.addEventListener("submit", function (event) {
             event.preventDefault();
 
             var firstName = firstNameInput.value;
             var lastName = lastNameInput.value;
 
-            var this_kid = document.querySelector('.kids_profile p');
-            this_kid.innerHTML = firstName + ' ' + lastName;
+            var this_kid = profile.querySelector(".kids_profile p");
+            this_kid.innerHTML = firstName + " " + lastName;
 
             closeForm();
         });
@@ -163,8 +171,5 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         }
         return null;
-}
-
-
-
-});//end
+    }
+});
