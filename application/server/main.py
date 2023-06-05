31ef8d6d2e2c9f1efa9808363d9b8eb98e35783d
joshlap <joshlapidario@gmail.com>
2023-06-05 13:27:49 -0700

    Revert "fixed case of empty locations database"
    This reverts commit 21feb55c78d1335160bd55293fdc6de3549a432b.

--- application/server/main.py
+++ application/server/main.py
@@ -202,7 +202,7 @@ def get_location(child_id: int):
 
     coordinates = db.select_coordinates(child_id)
 
-    if coordinates == {}: 
+    if coordinates == None: 
         return {"latitude": 0.0, "longitude": 0.0}
   
     latitude = coordinates['latitude']
@@ -324,7 +324,6 @@ def update_email(user_data:dict) -> dict:
 @app.put('/update_password')
 def update_password(user_data:dict) -> dict:
   return {'success': db.update_password(user_data['user_id'], user_data['password'])}
-
 
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # Main function
